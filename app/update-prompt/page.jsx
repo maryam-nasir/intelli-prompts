7cf4f1c8a09e4f687d143df83627a7b3ace1d1f2
Maryam Nasir <maryamnasir555@yahoo.com>
2024-02-19 20:01:16 +0500

    Uncommented code

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -6,54 +6,54 @@ import Form from "@components/Form";
 
 const UpdatePrompt = () => {
   const router = useRouter();
-  // const searchParams = useSearchParams();
-  // const promptId = searchParams.get("id");
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get("id");
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
     tag: "",
   });
 
-  // useEffect(() => {
-  //   const getPromptDetails = async () => {
-  //     const response = await fetch(`/api/prompt/${promptId}`);
-  //     const data = await response.json();
-  //     setPost({
-  //       prompt: data.prompt,
-  //       tag: data.tag,
-  //     });
-  //   };
+  useEffect(() => {
+    const getPromptDetails = async () => {
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
+    };
 
-  //   if (promptId) {
-  //     getPromptDetails();
-  //   }
-  // }, [promptId]);
+    // if (promptId) {
+      getPromptDetails();
+    // }
+  }, [promptId]);
 
   const updatePrompt = async (event) => {
     event.preventDefault();
-  //   setSubmitting(true);
+    setSubmitting(true);
 
-  //   if (!promptId) {
-  //     return alert("Prompt ID not found.");
-  //   }
+    if (!promptId) {
+      return alert("Prompt ID not found.");
+    }
 
-  //   try {
-  //     const response = await fetch(`/api/prompt/${promptId}`, {
-  //       method: "PATCH",
-  //       body: JSON.stringify({
-  //         prompt: post.prompt,
-  //         tag: post.tag,
-  //       }),
-  //     });
+    try {
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
 
-  //     if (response.ok) {
-  //       router.push("/");
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //   } finally {
-  //     setSubmitting(false);
-  //   }
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
   };
 
   return (
